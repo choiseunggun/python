dc0eb7b8aa935f0f122bd42ddef52c0e316869ca
sgchoi <sgchoi@future.co.kr>
2021-12-03 11:31:53 +0900

    수정 내용  - 윈도우 환경 대응  - API 정보 추가 : 분류, API 카운트 등

--- redoc_json_analyzer.py
+++ redoc_json_analyzer.py
@@ -7,7 +7,7 @@ import json
 if __name__ == '__main__':
     print('start')
 
-with open('/Users/choiseunggun/Downloads/openapi_127_0_0_1.json') as json_file:
+with open('C:\\Users\\최승건\\Desktop\\openapi_127_0_0_1.json', encoding='utf-8') as json_file:
     json_data = json.load(json_file)
 
     # json_array_info = json_data["info"]
@@ -18,14 +18,42 @@ with open('/Users/choiseunggun/Downloads/openapi_127_0_0_1.json') as json_file:
     json_array_info_paths = json_data["paths"]
     # print(json_array_info_paths)
 
-    cnt = len(json_array_info_paths);
+    # cnt = len(json_array_info_paths);
     # print('cnt : ',cnt)
 
-    cnt = 1;
+    api_cnt = 1;
     for key, value in json_array_info_paths.items():
+        method = ''
+        method_cnt = 0
+        method_tmp = []
+        key_split = key.split('/')
+        try:
+            depth1 = key_split[2]
+        except:
+            depth1 = ''
+        try:
+            depth2 = key_split[3]
+        except:
+            depth2 = ''
+
+        try:
+            depth3 = key_split[4]
+        except:
+            depth3 = ''
+
+        try:
+            depth4 = key_split[5]
+        except:
+            depth4 = ''
+
+        print(api_cnt, '"{}"'.format(key), '"{}"'.format(depth1), '"{}"'.format(depth2),
+              '"{}"'.format(depth3), '"{}"'.format(depth4), end=', ')
         for subkey, subvalue in value.items():
-            print(cnt, key,subkey,'"',subvalue['summary'],'"')
-            cnt=cnt+1
+            method_tmp.append(subkey+',')
+            method_cnt+=1
+        method = ''.join(method_tmp)
+        print('"{}"'.format(method[0:len(method)-1]), method_cnt, '"{}"'.format(subvalue['summary']) )
+        api_cnt += 1
 
     # json_array_info_components = json_data["components"]
     # print(json_array_info_components)
